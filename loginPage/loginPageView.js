b5126662df3280edea0922d290da5c4db03e337b
Oldskull-one <leobastoss@gmail.com>
2024-10-24 10:55:17 +0200

    login page
    login page

--- loginPage/loginPageView.js
+++ loginPage/loginPageView.js
@@ -1,25 +1,21 @@
-
-function loginView(){
-    document.getElementById('app').innerHTML= `
-    
-    <div class='form-input'>
-    <input
-    type='text'
-    placeholder='Enter username'
-    onchange= 'model.input.login.userName=this.value'
-    >
-    <input 
-    type='password'
-    placeholder='Enter passord'
-    onchange= 'model.input.login.password=this.value'
-    >
-    <button onclick="login()">Login</button>
-    <p>Har du ikke profil?
-    <span onclick='registerUser()'>Registrer her</span></p>
-    </div>
-    
-    
-    `
-   
+function loginView() {
+    document.getElementById('app').innerHTML = `
+        <div class="login-page">
+            <div class='form-input'>
+                <input
+                    type='text'
+                    placeholder='Enter username'
+                    onchange='model.input.login.userName=this.value'
+                />
+                <input 
+                    type='password'
+                    placeholder='Enter password'
+                    onchange='model.input.login.password=this.value'
+                />
+                <button onclick="login()">Login</button>
+                <p>Har du ikke profil? 
+                <span onclick='registerUser()'>Registrer her</span></p>
+            </div>
+        </div>
+    `;
 }
-
