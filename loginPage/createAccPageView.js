0e51f8da240087953d28b858a9208034e124b751
Oldskull-one <leobastoss@gmail.com>
2024-10-24 12:19:05 +0200

    new

--- loginPage/createAccPageView.js
+++ loginPage/createAccPageView.js
@@ -1,17 +1,18 @@
-function createAccView(){
-    document.getElementById('app').innerHTML= `
-    
-    <div class='form-input'>
-    <label for="email">Email:</label>
-    <input type="text" name="email">
-    <label for="username">Username:</label>
-    <input type="text" name="username">
-    <label for="password">Password:</label>
-    <input type="text" name="password">
-    <button onclick="signUp()">Register</button>
-    
-    </div>
-    
-    
-    `
+function createAccView() {
+    document.getElementById('app').innerHTML = `
+        <div class="register-page">
+            <div class='form-input'>
+                <label for="email">Email:</label>
+                <input type="text" name="email" placeholder="Enter your email">
+                
+                <label for="username">Username:</label>
+                <input type="text" name="username" placeholder="Choose a username">
+                
+                <label for="password">Password:</label>
+                <input type="password" name="password" placeholder="Create a password">
+                
+                <button onclick="signUp()">Register</button>
+            </div>
+        </div>
+    `;
 }
