d580821a3bb25c94e67c5b1e8d21fec741c7fb4b
haakonerol <haakon.erol@gmail.com>
2024-10-24 11:01:29 +0200

    accpageview updated

--- loginPage/createAccPageView.js
+++ loginPage/createAccPageView.js
@@ -2,10 +2,12 @@ function createAccView(){
     document.getElementById('app').innerHTML= `
     
     <div class='form-input'>
-    <input>
-    <input>
-    <input>
-    <input>
+    <label for="email">Email:</label>
+    <input type="text" name="email">
+    <label for="username">Username:</label>
+    <input type="text" name="username">
+    <label for="password">Password:</label>
+    <input type="text" name="password">
     <button onclick="signUp()">Register</button>
     
     </div>
